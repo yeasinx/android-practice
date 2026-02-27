f362e04248a7496a4caf130028eb6d4eff387ed7
yeaisnat <yeasinarafat.wa@gmail.com>
2026-02-27 15:04:03 +0600

    Tast 1: Article UI designing completed

--- ComposeActivity/app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ ComposeActivity/app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeActivityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   Article(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +36,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Article(modifier: Modifier = Modifier) {
+    val title = stringResource(R.string.article_title)
+    val para1 = stringResource(R.string.article_para1)
+    val para2 = stringResource(R.string.article_para2)
+
+    Column (modifier = modifier) {
+        ArticleImage()
+        ArticleText(
+            title = title,
+            para1 = para1,
+            para2 = para2
+        )
+    }
+}
+
+@Composable
+fun ArticleImage() {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Image(
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.FillWidth
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArticleText (title: String, para1: String, para2: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = para1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp, 0.dp)
+        )
+        Text(
+            text = para2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Article Preview")
+@Composable
+fun ArticlePreview() {
     ComposeActivityTheme {
-        Greeting("Android")
+        Scaffold() { innerPadding ->
+            Article(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
